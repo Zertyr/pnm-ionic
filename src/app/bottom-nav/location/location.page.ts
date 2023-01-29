1b8ed4b5fd540f9d1a3c33f6818fa150e16a0e8f
Damien <Damino95@gmail.com>
2023-01-29 12:47:16 +0100

    add map + marker + info popover

--- src/app/bottom-nav/location/location.page.ts
+++ src/app/bottom-nav/location/location.page.ts
@@ -1,8 +1,15 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { GoogleMap } from '@capacitor/google-maps';
 import { environment } from 'src/environments/environment';
+import { Geolocation } from '@capacitor/geolocation';
+import { HttpClient } from '@angular/common/http';
+import axios from 'axios';
+import { PopoverController } from '@ionic/angular';
+
+
 
 const apiKey = 'AIzaSyDsKgB1J3eZdEwP5oyorhnLbhfO9OZucQU';
+
 @Component({
   selector: 'app-location',
   templateUrl: 'location.page.html',
@@ -11,9 +18,14 @@ const apiKey = 'AIzaSyDsKgB1J3eZdEwP5oyorhnLbhfO9OZucQU';
 export class LocationPage {
   @ViewChild('map') mapRef: ElementRef<HTMLElement>;
   newMap: GoogleMap;
-
-
-  constructor() {
+  coordinates:GeolocationCoordinates;
+  places: Array<any> = [];
+  @ViewChild('popover') popover;
+  isPopoverOpen: Boolean = false;
+  title: string;
+  body: string;
+  
+  constructor(private http: HttpClient) {
   }
 
   async ionViewDidEnter(){
@@ -22,6 +34,9 @@ export class LocationPage {
 
   
   async createMap(){
+    this.coordinates = await (await Geolocation.getCurrentPosition()).coords;
+    console.log('Current position:', this.coordinates);
+
     this.newMap = await GoogleMap.create({
       id: 'my-map',
       apiKey: apiKey,
@@ -29,11 +44,75 @@ export class LocationPage {
       forceCreate: true,
       config: {
         center: {
-          lat: 33.6,
-          lng: -117.9
+          lat: this.coordinates.latitude,
+          lng: this.coordinates.longitude
         },
-        zoom:5
+        zoom:13
       }
     });
+    await this.newMap.enableClustering();
+    // Add a marker to the map
+    const markerId = await this.newMap.addMarker({
+      title: 'Your position',
+      coordinate: {
+        lat: this.coordinates.latitude,
+        lng: this.coordinates.longitude
+      }
+    });
+    this.GetPlace(this.coordinates);
+
+    this.newMap.setOnMarkerClickListener(async(marker) => {
+      console.log('marker : ' + JSON.stringify(marker));
+      this.isPopoverOpen = true;
+      this.title = marker.title;
+      this.body = marker.snippet;
+    });
   }
+
+  GetPlace(coordinates:GeolocationCoordinates){
+
+    var config = {
+      method: 'get',
+      url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ coordinates.latitude +'%2C'+ coordinates.longitude +'&radius=1500&type=car_rental&keyword=car_rental&key='+environment.YOUR_API_KEY,
+      headers: {
+        "Access-Control-Allow-Origin": '*',
+        "Access-Control-Allow-Methods": 'GET'
+       }
+    };
+
+    fetch(config.url)
+    .then(res => res.json())
+    .then(res => {
+      res.results.map((element, index) => {
+        const marketObj:any = {};
+        marketObj.id = index;
+        marketObj.name = element.name;
+        marketObj.photos = element.photos;
+        marketObj.rating = element.rating;
+        marketObj.vicinity = element.vicinity;
+        marketObj.marker = {
+          latitude: element.geometry.location.lat,
+          longitude: element.geometry.location.lng
+        };
+        console.log(marketObj);
+        this.places.push(marketObj);
+        console.log(this.places);
+
+      })
+    }).then(() => {
+      this.places.forEach(async (place) => {
+        console.log("place : " + JSON.stringify(place));
+        const markerId = await this.newMap.addMarker({
+          title: place.name,
+          snippet: JSON.stringify(place.vicinity),
+          coordinate: {
+            lat: place.marker.latitude,
+            lng: place.marker.longitude
+          }
+        })
+      });
+    })
+  }
+
+
 }
