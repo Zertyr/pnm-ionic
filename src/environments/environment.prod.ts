27fd082eb233bc31af41cd4791d79f4a788de2f1
Damien <Damino95@gmail.com>
2023-01-29 13:21:43 +0100

    fix leak

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,3 @@
 export const environment = {
-  production: true,
-  uriAPI: 'https://c128-2a01-cb0c-2ff-3a00-1d55-ad2c-4d12-79a9.eu.ngrok.io/api',
-  YOUR_API_KEY: 'AIzaSyDsKgB1J3eZdEwP5oyorhnLbhfO9OZucQU'
+  production: true
 };
